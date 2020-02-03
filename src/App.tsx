81f2d6af95e76f88d542352bc8cafed66cd7b22c
고은아 <kona@mpaycheck.com>
2020-02-03 16:15:15 +0900

    Add styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import React from "react";
-import "./App.css";
+import styled from "styled-components";
+
+interface ButtonTypes {
+  danger?: string;
+}
+
+const Button = styled.button<ButtonTypes>`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "red" : "greenyellow")};
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: silver;
+`;
 
 const App: React.FC = () => {
   return (
-    <>
-      <button className="button button--success">Hello</button>
-      <button className="button button--danger">Hello</button>
-    </>
+    <Container>
+      <Button />
+      <Button danger="danger" />
+    </Container>
   );
 };
 
